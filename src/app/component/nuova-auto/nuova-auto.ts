ccdc628ac140106ceeaaf97baf8888c53399886f
epero <eperoni76@gmail.com>
2026-01-07 15:22:58 +0100

    nuovi component

---
+++ src/app/component/nuova-auto/nuova-auto.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nuova-auto',
+  imports: [],
+  templateUrl: './nuova-auto.html',
+  styleUrl: './nuova-auto.scss',
+})
+export class NuovaAuto {
+
+}
